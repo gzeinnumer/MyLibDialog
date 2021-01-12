2462d87ded71cf4b0976fb770f3c4ee1938582ed
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-01-12 11:13:39 +0700

    README.md

--- app/src/main/java/com/gzeinnumer/mylibdialog/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibdialog/MainActivity.java
@@ -1,109 +1,77 @@
 package com.gzeinnumer.mylibdialog;
 
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gzeinnumer.mylibdialog.dialog.infoDialog.InfoDialog;
-import com.gzeinnumer.mylibdialog.dialog.numberPicker.NumberPickerDialog;
-
-//import com.gzeinnumer.mylibdialog.constant.ButtonStyle;
-//import com.gzeinnumer.mylibdialog.constant.DateScreenStyle;
-//import com.gzeinnumer.mylibdialog.constant.DialogType;
+import com.gzeinnumer.mylibdialog.dialog.loadingDialog.LoadingDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoadingDialog renewLoading;
+    InfoDialog renewInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+
+        //aktifkan salah satu
+        enableLoading();
+//        enableConfirm(); // jangan pakai. ada error
+    }
+
+    private void enableLoading() {
+        renewLoading = renewDialogLoading(1);
+        renewLoading.show();
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
+                renewLoading.dismis();
+                renewLoading = renewDialogLoading(2);
+                renewLoading.show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        renewLoading.dismis();
+                        renewLoading = renewDialogLoading(2);
+                        renewLoading.show();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                renewLoading.dismis();
+                            }
+                        }, 4000);
+                    }
+                }, 4000);
+            }
+        }, 4000);
+    }
 
-                new NumberPickerDialog(getSupportFragmentManager())
-                        .setAnimationStyle(R.style.DialogStyle_Slide)
-                        .setButtonAllCaps(false)
-                        .setTitle("ini title")
-                        .setContent("ini content").show();
+    private LoadingDialog renewDialogLoading(int i) {
+        return new LoadingDialog(getSupportFragmentManager())
+                .setContent(String.valueOf(i));
+    }
 
+    private void enableConfirm() {
+        renewInfo = renewDialogInfo(1);
+        renewInfo.show();
+        renewInfo.onOkPressedCallBack(new InfoDialog.OnOkPressed() {
+            @Override
+            public void onOkPressed() {
+                renewInfo.dismiss();
+                renewInfo = renewDialogInfo(2);
+                renewInfo.show();
             }
         });
-
-        new InfoDialog(getSupportFragmentManager())
-                .setCanceledOnTouchOutside(true)
-                .autoDismisOnSecond(10)
-                .show();
-
-//        _DatePickerDialogMultiNew();
-//        _TimePickerDialog();
     }
-//
-//    private void _DatePickerDialogSingleNew() {
-//        ((Button) findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new SingleDatePickerDialog(getSupportFragmentManager())
-//                        .setTheme(R.style.AppTheme2)
-//                        .setDialogScreen(DateScreenStyle.FullScreen)
-//                        .setTimeZone("GMT")
-//                        .setTitle("Pilih tanggal")
-//                        .setSelectedToday(true)
-//                        .setTimeFormat("dd/MM/yyyy")
-//                        .setStartDate("1/08/2020")
-//                        .setEndDate("31/12/2020")
-//                        .onOkPressedCallBack(new SingleDatePickerDialog.OnOkPressed() {
-//                            @Override
-//                            public void onOkPressed(String value) {
-//                                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
-//                            }
-//                        })
-//                        .build()
-//                        .show();
-//            }
-//        });
-//    }
 
-//    private void _DatePickerDialogMultiNew() {
-//        ((Button) findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new MultiDatePickerDialog(getSupportFragmentManager())
-//                        .setTheme(R.style.AppTheme2)
-//                        .setTimeZone("GMT")
-//                        .setTitle("Pilih tanggal")
-//                        .setTimeFormat("dd/MM/yyyy")
-//                        .setStartDate("1/08/2020")
-//                        .setEndDate("31/12/2020")
-//                        .onOkPressedCallBack(new MultiDatePickerDialog.OnOkPressed() {
-//                            @Override
-//                            public void onOkPressed(String firstDate, String secondDate) {
-//                                Toast.makeText(MainActivity.this, firstDate + " - " + secondDate, Toast.LENGTH_SHORT).show();
-//                            }
-//                        })
-//                        .build()
-//                        .show();
-//            }
-//        });
-//    }
-//
-//    private void _TimePickerDialog() {
-//        ((Button) findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                TimePickerDialog timePickerDialog = new TimePickerDialog(getSupportFragmentManager()).setTimeFormat(TimeFormat.CLOCK_24H).build();
-//
-//                timePickerDialog.show();
-//
-//                timePickerDialog.setListener(new TimePickerDialog.OnOkPressed() {
-//                    @Override
-//                    public void onOkPressed(TimeValueResult timeValueResult, String formated) {
-//                        Toast.makeText(getApplicationContext(), timeValueResult.getHour() + ":" + timeValueResult.getMinutes() + " / " + formated, Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//            }
-//        });
-//    }
+    private InfoDialog renewDialogInfo(int i) {
+        return new InfoDialog(getSupportFragmentManager())
+                .setContent(String.valueOf(i))
+                .autoDismisOnSecond(3);
+    }
 }
